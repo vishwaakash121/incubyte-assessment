872a6738dd16e4dbd8d07f901a4f33fcaeb75c7f
Akash Vishwakarma <vishwaakash12@gmail.com>
2024-09-03 21:35:56 +0530

    refactor delimiter parsing logic

--- string_calculator/lib/string_calculator.rb
+++ string_calculator/lib/string_calculator.rb
@@ -3,14 +3,21 @@ class StringCalculator
   #TODO method for addition
   def add(numbers)
     return 0 if numbers.empty?
-    
-    delimiters = /,|\n/
-    if numbers.start_with?("//")
-      delimiters = Regexp.escape(numbers[2])
-      numbers = numbers.split("\n", 2).last
-    end
 
-    numbers.split(/#{delimiters}/).map(&:to_i).sum
+    delimiters, numbers = _extract_delimiters(numbers)
+    numbers.split(delimiters).map(&:to_i).sum
+  end
+
+  private
+
+  def _extract_delimiters(numbers)
+    if numbers.start_with?("//")
+      delimiter = Regexp.escape(numbers[2])
+      numbers = numbers.split("\n", 2).last
+      [/#{delimiter}/, numbers]
+    else
+      [/,|\n/, numbers]
+    end
   end
 
 end
