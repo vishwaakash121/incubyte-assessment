e5029578e69e762ee1ab30da67b50da085db5522
Akash Vishwakarma <vishwaakash12@gmail.com>
2024-09-03 20:23:37 +0530

    For single and two numbers as input

--- string_calculator/spec/string_calculator_spec.rb
+++ string_calculator/spec/string_calculator_spec.rb
@@ -1,11 +1,21 @@
 require 'string_calculator'
 
 RSpec.describe StringCalculator do
-  
+
   describe '#add' do
     it 'returns 0 for an empty string' do
       calculator = StringCalculator.new
       expect(calculator.add("")).to eq(0)
+    end
+
+    it 'returns the same number when one number is given' do
+      calculator = StringCalculator.new
+      expect(calculator.add("3")).to eq(3)
+    end
+
+    it 'returns the sum of two numbers' do
+      calculator = StringCalculator.new
+      expect(calculator.add("3,2")).to eq(5)
     end
   end
 end
